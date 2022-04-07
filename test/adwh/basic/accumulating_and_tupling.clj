5689a9f9ef8303b094de95e7b8c8028061f27aac
Darong Mean <contact.darong@gmail.com>
2022-04-07 10:46:43 +0700

    F!! Implement fast steep

--- test/adwh/basic/accumulating_and_tupling.clj
+++ test/adwh/basic/accumulating_and_tupling.clj
@@ -19,14 +19,46 @@
 ;;;
 ;;; What is the running time and how can you improve it by tupling?
 
-(defn sum [xs]
+(defn sum
+  "O(n)"
+  [xs]
   (reduce + 0 xs))
 
-(defn steep [[x & xs]]
-  (if x
-    (and (< (sum xs) x) (steep xs))
-    true))
+(defn steep
+  "O(n^2)"
+  [[x & xs]]
+  (or (nil? x)
+      (and (< (sum xs) x)
+           (steep xs))))
 
 (defspec steep-false-test
   (prop/for-all [v (gen/return [3 2 1])]
     (is (not (steep v)))))
+
+(defspec steep-true-test
+  (prop/for-all [v (gen/return [4 2 1])]
+    (is (steep v))))
+
+;; Applying tupling optimization
+
+(defn fast-steep
+  "O(n)"
+  [[x & xs]]
+  (if-let [[sum steep?] (and x (fast-steep xs))]
+    [(+ sum x) (and (< sum x) steep?)]
+    [0 true]))
+
+(defn steep-02
+  "O(n)"
+  [xs]
+  (-> xs
+      fast-steep
+      second))
+
+(defspec fast-steep-false-test
+  (prop/for-all [v (gen/return [3 2 1])]
+    (is (not (steep-02 v)))))
+
+(defspec fast-steep-true-test
+  (prop/for-all [v (gen/return [4 2 1])]
+    (is (steep-02 v))))
