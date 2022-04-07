cbe36a4841ebd1b9e35a27227a605989a4b0be61
Darong Mean <contact.darong@gmail.com>
2022-04-07 23:19:25 +0700

    F!! Implement fold-concat fusion law

--- test/adwh/basic/fusion.clj
+++ test/adwh/basic/fusion.clj
@@ -12,6 +12,10 @@
   > g is strict
   > g (f y x) = h (g y) x
 
+  fold-concat fusion law(special case of fold fusion law):
+  > foldr f e · concat xss = foldr (foldr f x xs) e xss
+    where concat :: [[a]] -> [a]
+
   foldr fusion law (not applicable to Clojure since Clojure has no support for foldr):
   > g . foldr f e = foldr h (g e)
   provided that
@@ -80,3 +84,30 @@
   (prop/for-all [xs gen-xs]
     (is (= (->> xs (reduce f 0) g)
            (reduce h (g 0) xs)))))
+
+;;; fold-concat fusion law
+;;; foldr f e · concat xss = foldr (foldr f x xs) e xss
+;;; where concat :: [[a]] -> [a]
+(defn concat'
+  "Flatten one level of nested sequence."
+  [xss]
+  (reduce concat [] xss))
+
+(defspec foldl-concat-fusion-precondition-test
+  (let [;; given concat' definition: (reduce concat [] xss)
+        ;; apply foldl fusion law
+        ;; thus f = concat
+        f concat
+        g #(reduce - 0 %)
+        h (fn [acc xs] (reduce - acc xs))]
+    (prop/for-all [xs gen-xs
+                   ys gen-xs]
+      (is (= (g (f ys xs))
+             (h (g ys) xs))))))
+
+(defspec foldl-concat-fusion-test
+  (prop/for-all [xss (gen/vector gen-xs 0 3)]
+    (is (= (->> xss concat' (reduce - 0))
+           (reduce (fn [acc xs] (reduce - acc xs))
+                   0
+                   xss)))))
