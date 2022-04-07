10d60e075c064df80cdaf5bfe8121cac1d2d7cee
Darong Mean <contact.darong@gmail.com>
2022-04-07 23:59:52 +0700

    F!! Implement reverse using fold

---
+++ test/adwh/basic/fold.clj
@@ -0,0 +1,23 @@
+(ns adwh.basic.fold
+  "Most inductive definition can be expressed as instances of foldr.
+
+   The following idiom will be used frequently:
+   > foldr (concatMap . steps) e
+
+   Greedy and thinning algorithms are usually inductive."
+  (:require
+    [clojure.test :refer [is]]
+    [clojure.test.check.clojure-test :refer [defspec]]
+    [clojure.test.check.generators :as gen]
+    [clojure.test.check.properties :as prop]))
+
+(def gen-xs
+  (gen/vector gen/nat 0 3))
+
+;;; reverse as an instance of fold
+(defspec reverse-test
+  (prop/for-all [xs gen-xs]
+    (is (= (reverse xs)
+           (reduce (fn [acc x] (conj acc x))
+                   '()
+                   xs)))))
